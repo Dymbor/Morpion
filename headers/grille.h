8da8324dfe29accc85fa845e6f1dfd5b51fcd364
Dymbor <thaenfontaine@gmail.com>
2025-12-01 15:54:54 +0100

    Ajout conversion coordonnee

--- headers/grille.h
+++ headers/grille.h
@@ -9,11 +9,12 @@
 #define CROIX 1
 #define CERCLE 2
 
+void effaceConsole();
+void afficheCaseCouleur(int camp);
 void setGrilleVide(int grille[TAILLE][TAILLE]);
 void afficheGrille(int grille[TAILLE][TAILLE]);
-void afficheCaseCouleur(int camp);
-bool estCaseVide(int grille[TAILLE][TAILLE],int ligne, int colone);
-void effaceConsole();
-bool estCoupValide(int grille[TAILLE][TAILLE],int ligne, int colone);
+bool estCaseVide(int grille[TAILLE][TAILLE], int ligne, int colone);
+bool estCoupValide(int grille[TAILLE][TAILLE], int ligne, int colone);
+void convertitCoordonnees(char coordLetttre[2], int coordConvertie[2]);
 
 #endif
